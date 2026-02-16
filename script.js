1947c4a6f15021801709886d7af292be25a35af5
BABY <ravyllahauana@gmail.com>
2026-02-16 15:24:10 -0300

    Projeto landing page de lazer

---
+++ script.js
@@ -0,0 +1,6 @@
+const form = document.querySelector(".form-reserva")
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault()
+    alert(" Pedido enviado com sucesso! Em breve entraremos em contato.")
+});
