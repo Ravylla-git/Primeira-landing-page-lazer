e2457f9dbd7c08ae6db34610ad03699e3eeb92e2
BABY <ravyllahauana@gmail.com>
2026-03-09 13:46:48 -0300

    corrigindo o js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const form = document.querySelector(".form-reserva")
 
-form.addEventListener("submit", function(event) {
-    event.preventDefault()
+form.addEventListener("submit", function() {
+
     alert(" Pedido enviado com sucesso! Em breve entraremos em contato.")
 });
